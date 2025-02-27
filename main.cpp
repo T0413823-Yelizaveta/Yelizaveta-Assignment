a771c97774fed1824366656cae8a1663e1764b76
liza1 <T0413823@my.ntu.ac.uk>
2025-02-27 04:40:54 +0000

    Version1 1.2

--- main.cpp
+++ main.cpp
@@ -11,41 +11,122 @@ private:
 public:
     //Methods
     // Add item
-    void addItem(const string& itemName);
+    void addItem(const string& item) {
+        items.push_back(item);
+        cout << "You added to inventory: " << item << endl;
+    }
 
     // Remove
-    void removeItem(const string& itemName);
+    void removeItem(const string& item) {
+        for (auto it = items.begin(); it != items.end(); ++it) {
+            if (*it == item) {
+                items.erase(it);
+                cout<<"You removed from inventory: "<<item<<endl;
+                return;
+            }
+        }
+        cout << "Item " << item << "' is not found in inventory\n";
+    }
 
     // Check availability
-    bool hasItem(const string& itemName) const;
+    bool hasItem(const string& item) const {
+        for (const string& i : items) {
+            if (i == item) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     // output list items
-    void showItems() const;
+    void showItems() const {
+        if (items.empty()) {
+            cout << "Inventory is empty\n";
+            return;
+        }
+
+        cout << "Your Inventory:\n";
+        for (const string& item : items) {
+            cout << "- " << item << endl;
+        }
+    }
 };
 
 class Player {
 private:
-    string playerName;
-    int mentalHealth;
+    string name;
     int survivalRate;
+    int mentalHealth;
+
     bool isAlive;
     Inventory inventory;
 
 public:
-    Player(string name, int health, int survivalRate)
-{
-    cout << "Player Name: " << playerName << endl;
-}
-    // Methods for managing mental health
-    void increaseMentalHealth(int amount);
-    void decreaseMentalHealth(int damage);
+    Player(string PlayerName) {
+        name = PlayerName;
+        survivalRate = 100;
+    }
 
+    string getName() {
+        return name;
+    }
+    int getmentalHealth() {
+        return mentalHealth;
+    }
+    int getSurvivalRate() {
+        return survivalRate;
+    }
+
+    void sayHello() {
+        cout << "Hello my name is" << name<<"!"<< endl;
+    }
+    //Methods for managing mental health
+    void increaseMentalHealth(int amount) {
+        mentalHealth += amount;
+        if (mentalHealth > 100) {
+            mentalHealth = 100;
+        }
+        cout << "Your mental health " << mentalHealth << "%" << endl;
+    }
+
+    // Decrease mental health (if < 10%, playues failed)
+    void decreaseMentalHealth(int damage) {
+        mentalHealth -= damage;
+        if (mentalHealth < 10) {
+            cout << "Mental health is critical. You failed..." << endl;
+        } else {
+            cout << "Mental health is descreased: " << mentalHealth << "%" << endl;
+        }
+    }
     // Methods for managing SurvivalRate
-    void increaseSurvivalRate(int amount);
-    void decreaseSurvivalRate(int amount);
+    void increaseSurvivalRate(int amount) {
+        survivalRate += amount;
+        if (survivalRate > 100) {
+            survivalRate = 100;
+        }
+        cout<<"Survival Rate: "<<survivalRate<<"%"<<endl;
+    }
+
+
+    void decreaseSurvivalRate(int amount) {
+        survivalRate -= amount;
+        if (survivalRate < 0) {
+            survivalRate = 0;
+        }
+        cout<<"Survival Rate: "<<survivalRate<<"%"<<endl;
+    }
 
     // Check status
-    void checkStatus() const;
+    void showStatus() {
+        cout<<"Player state: "<<isAlive<<endl;
+        cout<<"\nSurvival Rate: "<<survivalRate<<"%";
+    }
+
+    // Direct access to `inventory`
+    Inventory& getInventory() {
+        return inventory;
+    }
+
 };
 
 
@@ -62,13 +143,86 @@ class Event {
 };
 
 
+class Scene {
+private:
+
+
+public:
+    Scene(string desc);
+    string description;
+
+
+};
+
+
+class CombatScene : public Scene {
+private:
+    int damage;
+    int mentalHealth;
+
+public:
+    CombatScene(string desc, int dmg, int mentalHealth);
+
+    void play(Player& player) {
+        cout << "\n" << description << endl;
+        cout << "Your attempt ...\n";
+
+        int attempt = 1;
+        if (attempt==1) {
+            cout << "You succesfully dodged. No damage\n";
+        }
+        cout << "You didn't have time to dodge - " << damage << "% Mental health.\n";
+        player.decreaseMentalHealth(damage);
+        }
+};
+
+
+
+class PuzzleScene : public Scene {
+private:
+    string question;
+    string correctAnswer;
+    int survivalBonus;
+
+public:
+    PuzzleScene(string desc, string q, string answer, int bonus);
+    void play(Player& player) {
+        cout << "\n" << description << endl;
+        cout << "Quiz: " << question << "\nYour answer: ";
+
+        string userAnswer;
+        cin >> userAnswer;
+
+        if (userAnswer == correctAnswer) {
+            cout << "Right! + " << survivalBonus << "% to chance to survival rate.\n";
+            player.increaseSurvivalRate(survivalBonus);
+        } else {
+            cout << "Wrong answer.No bonuses.\n";
+        }
+    }
+};
+
 
 int main() {
 
-    string playerName;
-    cout << "Enter your name: ";
-    cin >> playerName;
-    cout << "Добро пожаловать в игру, " << playerName << "!";
+    string inputName;
+    cout << "Enter your character name: ";
+    cin >> inputName;
+
+    Player player1(inputName);//Create an object with name
+    cout<<"The player saved as: "<<player1.getName()<<endl;
+
+    player1.sayHello();//Show the greetings
+    player1.showStatus();
+
+
+
+
+
+    return 0;
+
+
+
 
 }
 
